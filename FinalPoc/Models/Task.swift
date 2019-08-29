cd7ca985366561d3296b70c1e258010cb8386fef
Elias Paulino <eliaspaulinoandrade@gmail.com>
2019-08-29 12:06:43 -0300

    Add label, pickerView

---
+++ FinalPoc/Models/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  FinalPoc
+//
+//  Created by Elias Paulino on 29/08/19.
+//  Copyright © 2019 Elias Paulino. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    var name: String
+    var spendTime: (hour: Int, minutes: Int)
+}
---
+++ FinalPoc/ViewControllers/AddTaskViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddTaskViewController.swift
+//  FinalPoc
+//
+//  Created by Elias Paulino on 29/08/19.
+//  Copyright © 2019 Elias Paulino. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var timePickerView: UIPickerView! {
+        didSet {
+            timePickerView.delegate = self
+            timePickerView.dataSource = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
+
+extension AddTaskViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 24
+        }
+        return 60
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row) rf"
+    }
+    
+}
